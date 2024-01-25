904c08025da38e71944d276afd6ec5ad8ebcb94d
Emil Dinchev <emil.dinchev@sap.com>
2024-01-25 15:25:17 +0100

    Restore the mixins with JsonInclude.annotations

---
+++ dataformat-json/src/main/java/org/eclipse/digitaltwin/aas4j/v3/dataformat/json/mixins/ReferenceMixin.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2021 Fraunhofer-Gesellschaft zur Foerderung der angewandten Forschung e. V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eclipse.digitaltwin.aas4j.v3.dataformat.json.mixins;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.eclipse.digitaltwin.aas4j.v3.model.Key;
+import org.eclipse.digitaltwin.aas4j.v3.model.ReferenceTypes;
+
+import java.util.List;
+
+public interface ReferenceMixin {
+    @JsonInclude(JsonInclude.Include.ALWAYS)
+    @JsonProperty("keys")
+    List<Key> getKeys();
+
+    @JsonInclude(JsonInclude.Include.ALWAYS)
+    @JsonProperty("type")
+    ReferenceTypes getType();
+
+    @JsonProperty("type")
+    void setType(ReferenceTypes type);
+}
