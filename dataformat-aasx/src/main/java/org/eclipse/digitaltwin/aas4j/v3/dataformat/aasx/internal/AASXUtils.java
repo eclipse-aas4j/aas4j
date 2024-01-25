28d1a71482470b5ffdc05a739ff1177cabf728a6
Emil Dinchev <emil.dinchev@sap.com>
2024-01-25 16:18:47 +0100

    Add copyright headers

--- dataformat-aasx/src/main/java/org/eclipse/digitaltwin/aas4j/v3/dataformat/aasx/internal/AASXUtils.java
+++ dataformat-aasx/src/main/java/org/eclipse/digitaltwin/aas4j/v3/dataformat/aasx/internal/AASXUtils.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2024 Fraunhofer-Gesellschaft zur Foerderung der angewandten Forschung e. V.
- *
+ * Copyright (C) 2023 SAP SE or an SAP affiliate company.
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
