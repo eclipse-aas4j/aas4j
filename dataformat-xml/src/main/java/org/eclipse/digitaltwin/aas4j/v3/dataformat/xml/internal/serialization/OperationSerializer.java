843397f0c63eca97aaeae1db8551c94173e6c1d5
Hannes Stiebitzhofer <hannes@stiebitzhofer.com>
2026-01-14 14:17:37 +0100

    Fix XML Operation serialization/deserialization wrappers (#448)

--- dataformat-xml/src/main/java/org/eclipse/digitaltwin/aas4j/v3/dataformat/xml/internal/serialization/OperationSerializer.java
+++ dataformat-xml/src/main/java/org/eclipse/digitaltwin/aas4j/v3/dataformat/xml/internal/serialization/OperationSerializer.java
@@ -21,7 +21,10 @@ import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import java.io.IOException;
 import java.util.List;
-import org.eclipse.digitaltwin.aas4j.v3.model.*;
+import org.eclipse.digitaltwin.aas4j.v3.model.LangStringNameType;
+import org.eclipse.digitaltwin.aas4j.v3.model.LangStringTextType;
+import org.eclipse.digitaltwin.aas4j.v3.model.Operation;
+import org.eclipse.digitaltwin.aas4j.v3.model.OperationVariable;
 
 public class OperationSerializer extends JsonSerializer<Operation> {
 
@@ -29,35 +32,40 @@ public class OperationSerializer extends JsonSerializer<Operation> {
   public void serialize(Operation operation, JsonGenerator gen, SerializerProvider serializers)
       throws IOException {
     gen.writeStartObject();
+    writeWrappedList(gen, serializers, "extensions", "extension", operation.getExtensions());
     if (operation.getCategory() != null) {
       gen.writeStringField("category", operation.getCategory());
     }
     if (operation.getIdShort() != null) {
       gen.writeStringField("idShort", operation.getIdShort());
     }
+    List<LangStringNameType> displayName = operation.getDisplayName();
+    if (displayName != null && !displayName.isEmpty()) {
+      gen.writeFieldName("displayName");
+      new LangStringsNameTypeSerializer().serialize(displayName, gen, serializers);
+    }
     List<LangStringTextType> description = operation.getDescription();
     if (description != null && !description.isEmpty()) {
-      if (description.size() == 1) {
-        gen.writeFieldName("description");
-        gen.writeStartObject();
-        gen.writeFieldName("langStringTextType");
-        serializers.defaultSerializeValue(description.get(0), gen);
-        gen.writeEndObject();
-      } else {
-        gen.writeArrayFieldStart("description");
-        gen.writeStartObject();
-        for (LangStringTextType desc : description) {
-          gen.writeFieldName("langStringTextType");
-          serializers.defaultSerializeValue(desc, gen);
-        }
-        gen.writeEndObject();
-        gen.writeEndArray();
-      }
+      gen.writeFieldName("description");
+      new LangStringsTextTypeSerializer().serialize(description, gen, serializers);
     }
     if (operation.getSemanticId() != null) {
       gen.writeFieldName("semanticId");
       serializers.defaultSerializeValue(operation.getSemanticId(), gen);
     }
+    writeWrappedList(
+        gen,
+        serializers,
+        "supplementalSemanticIds",
+        "reference",
+        operation.getSupplementalSemanticIds());
+    writeWrappedList(gen, serializers, "qualifiers", "qualifier", operation.getQualifiers());
+    writeWrappedList(
+        gen,
+        serializers,
+        "embeddedDataSpecifications",
+        "embeddedDataSpecification",
+        operation.getEmbeddedDataSpecifications());
 
     List<OperationVariable> inputVariables = operation.getInputVariables();
     if (inputVariables != null && !inputVariables.isEmpty()) {
@@ -74,80 +82,6 @@ public class OperationSerializer extends JsonSerializer<Operation> {
       serializeOperationVariable(gen, serializers, inoutputVariables, "inoutputVariables");
     }
 
-    List<EmbeddedDataSpecification> edsList = operation.getEmbeddedDataSpecifications();
-    if (edsList != null && !edsList.isEmpty()) {
-      if (edsList.size() == 1) {
-        gen.writeFieldName("embeddedDataSpecification");
-        serializers.defaultSerializeValue(edsList.get(0), gen);
-      } else {
-        gen.writeArrayFieldStart("embeddedDataSpecification");
-        for (EmbeddedDataSpecification eds : edsList) {
-          serializers.defaultSerializeValue(eds, gen);
-        }
-        gen.writeEndArray();
-      }
-    }
-
-    // extensions
-    List<Extension> extensions = operation.getExtensions();
-    if (extensions != null && !extensions.isEmpty()) {
-      if (extensions.size() == 1) {
-        gen.writeFieldName("extensions");
-        serializers.defaultSerializeValue(extensions.get(0), gen);
-      } else {
-        gen.writeArrayFieldStart("extensions");
-        for (Extension ext : extensions) {
-          serializers.defaultSerializeValue(ext, gen);
-        }
-        gen.writeEndArray();
-      }
-    }
-
-    // supplementalSemanticIds
-    List<Reference> suppSemanticIds = operation.getSupplementalSemanticIds();
-    if (suppSemanticIds != null && !suppSemanticIds.isEmpty()) {
-      if (suppSemanticIds.size() == 1) {
-        gen.writeFieldName("supplementalSemanticIds");
-        serializers.defaultSerializeValue(suppSemanticIds.get(0), gen);
-      } else {
-        gen.writeArrayFieldStart("supplementalSemanticIds");
-        for (Reference ref : suppSemanticIds) {
-          serializers.defaultSerializeValue(ref, gen);
-        }
-        gen.writeEndArray();
-      }
-    }
-
-    // qualifiers
-    List<Qualifier> qualifiers = operation.getQualifiers();
-    if (qualifiers != null && !qualifiers.isEmpty()) {
-      if (qualifiers.size() == 1) {
-        gen.writeFieldName("qualifiers");
-        serializers.defaultSerializeValue(qualifiers.get(0), gen);
-      } else {
-        gen.writeArrayFieldStart("qualifiers");
-        for (Qualifier qualifier : qualifiers) {
-          serializers.defaultSerializeValue(qualifier, gen);
-        }
-        gen.writeEndArray();
-      }
-    }
-
-    // displayName
-    List<LangStringNameType> displayName = operation.getDisplayName();
-    if (displayName != null && !displayName.isEmpty()) {
-      if (displayName.size() == 1) {
-        gen.writeFieldName("displayName");
-        serializers.defaultSerializeValue(displayName.get(0), gen);
-      } else {
-        gen.writeArrayFieldStart("displayName");
-        for (LangStringNameType dn : displayName) {
-          serializers.defaultSerializeValue(dn, gen);
-        }
-        gen.writeEndArray();
-      }
-    }
-
     gen.writeEndObject();
   }
 
@@ -162,6 +96,26 @@ public class OperationSerializer extends JsonSerializer<Operation> {
     gen.writeArrayFieldStart("operationVariable");
     for (OperationVariable var : inputVariables) {
       serializers.defaultSerializeValue(var, gen);
+    }
+    gen.writeEndArray();
+    gen.writeEndObject();
+  }
+
+  private static <T> void writeWrappedList(
+      JsonGenerator gen,
+      SerializerProvider serializers,
+      String wrapperName,
+      String itemName,
+      List<T> values)
+      throws IOException {
+    if (values == null || values.isEmpty()) {
+      return;
+    }
+    gen.writeFieldName(wrapperName);
+    gen.writeStartObject();
+    gen.writeArrayFieldStart(itemName);
+    for (T value : values) {
+      serializers.defaultSerializeValue(value, gen);
     }
     gen.writeEndArray();
     gen.writeEndObject();
